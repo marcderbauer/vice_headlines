67609d6e2a14b59a387021777e074509a5ed860b
marcderbauer <marcanthony.bauer@gmail.com>
2022-08-27 12:21:34 +0200

    getting ready for merge

---
+++ generate.py
@@ -0,0 +1,3 @@
+# Generate new sentences sampled from the language model
+# Are any of the functions here used for validation? => How dependent is this on training?
+# Or is this just as a way to easily generate sentences after training?
