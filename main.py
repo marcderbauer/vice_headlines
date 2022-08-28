240cf49447bc33b144e6c77426b514813408d5ee
marcderbauer <marcanthony.bauer@gmail.com>
2022-08-28 10:57:45 +0200

    code formatting and minor adjustments

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-# Train and validation go in here, making use of the data and model
+#!/usr/bin/env python
+# To run this as a background process:
+# python main.py &
+# Might to manually kill the process when you're done, otherwise it will stay active in the background
+
+# TODO:
 # Could implement batching data here
 
 import time
@@ -14,27 +19,32 @@ from torch.utils.data import random_split
 from data import Data
 import model
 
-LOG_WANDB = False
+# -----------------------------------------------------------------------------------------#
+#                                           PARAMS                                         #
+# -----------------------------------------------------------------------------------------#
 
-DATA_LOCATION = "data/overfit_char.txt"#data/names/*.txt"
+# General Parameters
+DATA_LOCATION = "data/names/*.txt" # "data/overfit_char.txt"
 MODELS_DIR = "models"
-SAVE_DIR = os.path.join(MODELS_DIR, os.path.basename(DATA_LOCATION).split(".")[0]) #"models/names"
-if not os.path.exists(SAVE_DIR):
-    os.mkdir(SAVE_DIR)
-
+LEVEL = "char"              # whether the model operates at a "char" or "word" level
+LOG_WANDB = True
 SEED = None
+SAVE_EVERY = 1              # Save every X epochs (aside from best model)
+LOG_ITER = 500              # Log every X steps
 
-LEARNING_RATE = 0.0005 # in wlm github it's 20??
+# Model Parameters
+LEARNING_RATE = 0.0005      # in wlm github it's 20??
 N_EPOCHS = 100000           # Number of epochs
 MAX_LENGTH = 20             # Max input length
 HIDDEN_SIZE = 128           # Size of hidden Layer
-SAVE_EVERY = 10             # Save every X epochs (aside from best model)
-LOG_ITER = 500              # Log every X steps
 CRITERION = nn.NLLLoss()    # Loss function used
 CLIP = 0.25                 # Gradient clipping
-LEVEL = "char"
 
+# -----------------------------------------------------------------------------------------#
+#                                           SETUP                                          #
+# -----------------------------------------------------------------------------------------#
 
+# Set W and B logging
 if LOG_WANDB:
     wandb.init(project="vice_headlines", entity="marcderbauer")
 
@@ -44,19 +54,26 @@ if LOG_WANDB:
     "batch_size": 1
     }
 
-# SETUP
+# Generate and set seed
 if not SEED:
-    SEED = random.randrange(10000)
+    SEED = random.randrange(100000)
     print(f"No seed set. Current seed: {SEED}\n")
 torch.manual_seed(SEED)
 
+# Create directory to save models in
+SAVE_DIR = os.path.join(MODELS_DIR, os.path.basename(DATA_LOCATION).split(".")[0]) #"models/names"
+if not os.path.exists(SAVE_DIR):
+    os.mkdir(SAVE_DIR)
 
+# Load data and split into train and test
 data = Data(DATA_LOCATION, level=LEVEL)
 train_data, test_data = random_split(data, [round(len(data) * 0.8), round(len(data) * 0.2)])
-
 num_tokens = data.num_words
-model = model.RNN(num_tokens, HIDDEN_SIZE, num_tokens, num_layers=None, num_categories=data.num_categories)
+
+# Load the model
 # TODO: look into num_layers
+model = model.RNN(num_tokens, HIDDEN_SIZE, num_tokens, num_layers=None, num_categories=data.num_categories)
+
 
 
 # TRAINING
@@ -82,7 +99,6 @@ def evaluate(data_source):
 def train(model, category_tensor, input_line_tensor, target_line_tensor):
     """
     Single training step from the model side. 
-    # TODO: Should this not be an entire epoch worth of training? Would that be better?
     """
     target_line_tensor.unsqueeze_(-1) # Arranges data in a vertical tensor
     hidden = model.initHidden() # is the hidden state re-initialized every epoch?
@@ -122,9 +138,8 @@ def main():
 
             for i, example in enumerate(train_data):
                     
-                output, train_loss = train(model, *example)
-                #eval_loss = evaluate(test_data)
-                eval_loss = 0
+                _, train_loss = train(model, *example)
+                eval_loss = evaluate(test_data)
 
                 total_train_loss += train_loss
                 total_eval_loss += eval_loss
@@ -139,7 +154,7 @@ def main():
             train_loss = epoch_train_loss / len(train_data)
             eval_loss = epoch_eval_loss / len(train_data)
 
-            # TODO: Change to save and print every epoch, could print every 1k iterations or whatever
+
             if epoch % SAVE_EVERY == 0:
                 print('%s (%d %d%%) train: %.4f   eval: %.4f' % (timeSince(start), epoch, epoch / N_EPOCHS * 100, train_loss, eval_loss))
                 with open(os.path.join(SAVE_DIR, f"{epoch}.pt"), 'wb') as f:
